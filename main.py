28e78252250908ce15cbb4c9ac418f7e87062fd4
xinroul <77480188+xrlim97@users.noreply.github.com>
2022-10-03 22:13:46 +0800

    upz

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-from flask import Flask
-from flask_restful import Resource, Api
+from fastapi import FastAPI
 
-app = Flask(__name__)
-api = Api(app)
+app = FastAPI()
 
-class HelloWorld(Resource):
-    def get(self):
-        return "Hello Python!"
 
-api.add_resource(HelloWorld, '/')
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+@app.get("/")
+async def root():
+   return {"message": "Hello World"}
